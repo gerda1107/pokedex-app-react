6e3ee6f0e9ce69e647d9d2eeba3ee105906b48ce
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-26 14:50:00 +0300

    able to fetch data from API endpoint and store it

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,7 +1,25 @@
 import * as React from "react";
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 const PokemonList = () => {
-  return <div>Pokemon list goes here</div>;
+
+  const [getPokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+        fetchList();
+  }, [])
+  
+  const fetchList = async () => {
+    await Axios.get('https://pokeapi.co/api/v2/pokemon?limit=15')
+      .then(res => { setPokemon(res.data.results); })
+      .finally(() => setLoading(false));
+  }
+
+  console.log(getPokemon);
+
+  return loading ? (<div>No Pokemon</div>) : (<div>Pokemon list goes here</div>);
 };
 
 export default PokemonList;
