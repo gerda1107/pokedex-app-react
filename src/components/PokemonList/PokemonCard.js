62d305e271ca0d61231ed096af718f683c970bb3
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-27 08:05:00 +0300

    fetching more data w/specific pokemon URL

--- src/components/PokemonList/PokemonCard.js
+++ src/components/PokemonList/PokemonCard.js
@@ -1,6 +1,29 @@
 import * as React from "react";
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+
 const PokemonDetails = ({ pokemonInfo }) => {
-    return <div>{pokemonInfo.name}</div>;
+
+    const [getPokemon, setPokemon] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchPokemon();   
+    }, []);
+    
+    const fetchPokemon = async () => {
+    await Axios.get(`${pokemonInfo.url}`)
+        .then(res => {
+            setPokemon(res.data);
+        })
+        .finally(() => setLoading(false));
+    }
+    
+    return loading ? (<div>no data</div>) :
+        (<div>
+        <h3>{pokemonInfo.name}</h3>
+        <img src={getPokemon.sprites.front_default} alt=""></img>
+    </div>);
 };
 
 export default PokemonDetails;
