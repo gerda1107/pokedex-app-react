738a9d615762f5e63b447dbae72e157f192c85b8
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-27 20:03:19 +0300

    list and details routes added to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import PokemonList from "./components/PokemonList/PokemonList";
+import PokemonDetails from "./components/PokemonDetails/PokemonDetails";
 
 export default function App() {
-  return <PokemonList />;
+
+  return <Router>
+    <Switch>
+      <Route exact path="/">
+        <PokemonList />
+      </Route>
+      <Route exact path="/pokemon/:index">
+        <PokemonDetails />
+      </Route>
+    </Switch>
+  </Router>
 }
