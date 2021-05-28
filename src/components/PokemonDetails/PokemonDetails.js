4cba59b11c4d23c25f2b0689fd38b01d9be48e7d
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-28 08:22:10 +0300

    pokemonDetails in progress, able to fetch pokemon data

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -1,7 +1,29 @@
 import * as React from "react";
+import { useState, useEffect } from 'react';
+import { fetchData } from '../PokemonList/fetchData';
 
 const PokemonDetails = () => {
-  return <div>Pokemon details page goes here</div>;
+
+  const pokemonId = window.location.pathname.split('/').pop();
+
+  const [getDetails, setDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      fetchDetails(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    }, [])
+
+    const fetchDetails = async(url) => {
+        const { data: Details } = await fetchData(url);
+        if (Details) {
+        setDetails(Details);
+        setLoading(false);
+        }
+  }
+  
+  return loading ?
+    (<div className="spinner"></div>) :
+    (<div>dsfsdf</div>);
 };
 
 export default PokemonDetails;
