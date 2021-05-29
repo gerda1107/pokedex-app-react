1f78f85bb0aac291eb848e248927c9b96269db44
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-29 17:52:11 +0300

    updated favoriteIcon to avoid lint err in log

--- src/components/FavoriteIcon/FavoriteIcon.js
+++ src/components/FavoriteIcon/FavoriteIcon.js
@@ -7,12 +7,12 @@ const FavoriteIcon = () => {
       viewBox="0 0 300 275"
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
-      class="star"
+      className="star"
     >
       <polygon
         fill="none"
         stroke="#eee"
-        stroke-width="20"
+        strokeWidth="20"
         points="150,25 179,111 269,111 197,165  223,251 150,200 77,251 103,165 31,111 121,111"
       />
     </svg>
