a4d2e590583ec8c4b521ea6bc129be76dc66a678
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-30 15:43:26 +0300

    clean up

--- src/components/HandleLocalStorage/handleLocalStorage.js
+++ src/components/HandleLocalStorage/handleLocalStorage.js
@@ -7,7 +7,7 @@ export const doesKeyExist = (key) => {
 }
 
 export const parseData = (key) => {
-    let parsed = JSON.parse(JSON.parse(localStorage.getItem(key)));
+    let parsed = JSON.parse(localStorage.getItem(key));
     if (typeof parsed === 'string') parsed = JSON.parse(parsed);
     return parsed;
 }
--- src/components/PokemonList/PokemonCard.js
+++ src/components/PokemonList/PokemonCard.js
@@ -1,23 +1,19 @@
 import * as React from "react";
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FetchPokemon } from '../HandleFetch/fetchData';
-import FavoriteIcon from '../FavoriteIcon/FavoriteIcon';
 import { parseData } from '../HandleLocalStorage/handleLocalStorage';
 
-const PokemonDetails = ({ pokemonInfo }) => {
-
-  const [getPokemon, setPokemon] = useState(parseData(pokemonInfo.name));
+const PokemonCard = ({ pokemonInfo }) => {
+  const getPokemon = parseData(pokemonInfo.name);
   
   FetchPokemon(pokemonInfo.url, pokemonInfo.name);
 
-    return <Link to={`pokemon/${getPokemon.name}`} className="text-link pokemon-card">
+  return <Link to={`pokemon/${getPokemon.name}`} className="text-link">
           <div className="d-flex jc-space-evenly">
             <img src={getPokemon.sprites.front_default} alt=""></img>
-            <div className="favorite-icon"><FavoriteIcon/></div>
           </div>
           <h3>{pokemonInfo.name.toUpperCase()}</h3>
-      </Link>
+  </Link>
 };
 
-export default PokemonDetails;
+export default PokemonCard;
