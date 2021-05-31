5ab64d9c9a391ebd19b04fbc190a4c99340078ea
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-31 09:22:44 +0300

    PokemonCard.js updated to take data from localStorage

--- src/components/PokemonList/PokemonCard.js
+++ src/components/PokemonList/PokemonCard.js
@@ -1,18 +1,15 @@
 import * as React from "react";
 import { Link } from 'react-router-dom';
-import { FetchPokemon } from '../HandleFetch/fetchData';
 import { parseData } from '../HandleLocalStorage/handleLocalStorage';
 
-const PokemonCard = ({ pokemonInfo }) => {
-  const getPokemon = parseData(pokemonInfo.name);
-  
-  FetchPokemon(pokemonInfo.url, pokemonInfo.name);
+const PokemonCard = ({ pokemonName }) => {  
+  const getPokemon = parseData(pokemonName);
 
-  return <Link to={`pokemon/${getPokemon.name}`} className="text-link">
+  return <Link to={`pokemon/${pokemonName}`} className="text-link">
           <div className="d-flex jc-space-evenly">
             <img src={getPokemon.sprites.front_default} alt=""></img>
           </div>
-          <h3>{pokemonInfo.name.toUpperCase()}</h3>
+          <h3>{pokemonName.toUpperCase()}</h3>
   </Link>
 };
 
