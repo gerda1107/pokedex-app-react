bb23d1ed4fe94145ef2333f1f3e7f568efff4161
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-31 09:22:21 +0300

    PokemonList.js updated accordingly to fetcData.js to fetch data and setLoading to false once all info loaded

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,30 +1,34 @@
 import * as React from "react";
 import { useState, useEffect } from 'react';
-import { fetchData } from './fetchData';
+import { fetchData } from '../HandleFetch/fetchData';
 import PokemonCard from './PokemonCard';
+import { parseData } from '../HandleLocalStorage/handleLocalStorage';
+import FavoriteIcon from '../FavoriteIcon/FavoriteIcon';
 
-const PokemonList = () => {
+const PokemonList = ({addToFavorite, favorites}) => {
 
-  const [getPokemons, setPokemons] = useState(null);
+  const [getPokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
+  const pokemons = parseData('pokemon_list');
 
   useEffect(() => {
-    fetchPokemons('https://pokeapi.co/api/v2/pokemon?limit=15');
-  }, []);
-
-  const fetchPokemons = async(url) => {
-    const { data: Pokemons } = await fetchData(url);
-    if (Pokemons) {
-      setPokemons(Pokemons.results);
+    if (pokemons !== null) {
+      setPokemons([...pokemons]);
       setLoading(false);
     }
-  }
+    fetchData();
+  }, [])
 
   return loading ?
     (<div className="spinner"></div>) :
     (<div className="pokemon-container d-flex">
       {getPokemons.map((pokemon, index) =>
-      { return <PokemonCard pokemonInfo={pokemon} key={index} /> }
+      {return <div className="pokemon-card" key={index}>
+        <PokemonCard pokemonName={pokemon.name} addToFavorite={addToFavorite} />
+        <div className="favorite-icon" onClick={() => addToFavorite(pokemon.name)}>
+        {favorites.includes(pokemon.name) ?<FavoriteIcon fill={'white'} /> : <FavoriteIcon fill={'none'}/>}</div>
+      </div>
+      }
     )}</div>);
 };
 
